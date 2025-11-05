7720a0a08878bd1bc5ab20ee1eaefd5fb4321372
kadbshiz <leinash1zuk0@gmail.com>
2025-11-05 16:59:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import mymodule
+
+mymodule.say_hello("Леина")
+result = mymodule.add_numbers(2, 3)
+print("2 + 3 =", result)
+
+input("Нажми Enter, чтобы закрыть...")
---
+++ mymodule.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    print(f"Привет, {name}!")
+
+def add_numbers(a, b):
+    return a + b
